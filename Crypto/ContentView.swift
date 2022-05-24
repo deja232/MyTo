8eb4168edad56a263648c4f147224607558c3c8c
deja232 <84335792+deja232@users.noreply.github.com>
2022-05-24 08:51:17 +0700

    Update ContentView.swift

--- Crypto/ContentView.swift
+++ Crypto/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack(){
+            Text("Crypto")
+                .font(.title)
+                .fontWeight(.heavy)
+            Spacer()
+        }
+        .offset(y:-250)
+        .padding()
     }
 }
 
